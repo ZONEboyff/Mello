5b202ac9060eb186bb20d795ada6593548d00bd1
Pratik Khirod Nayak <pratik18karate12345@gmail.com>
2025-01-09 22:46:02 +0530

    added the splash screen

---
+++ app/src/main/java/com/example/mello/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.mello
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
+        val tvAppName: TextView = findViewById(R.id.tv_app_name)
+        tvAppName.typeface = typeFace
+    }
+}
